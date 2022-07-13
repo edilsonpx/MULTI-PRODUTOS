2e5caf2cdea053605605a5d084f315e9ce804be5
edilson <94394633+edilsonpx@users.noreply.github.com>
2022-07-13 00:30:37 -0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+
+var thumb1 =  document.getElementById("thumb1");
+var thumb1Src =  document.getElementById("thumb1").src;
+var thumb2 =  document.getElementById("thumb2");
+var thumb2Src =  document.getElementById("thumb2").src;
+var thumb3 =  document.getElementById("thumb3");
+var thumb3Src =  document.getElementById("thumb3").src;
+var thumb4 =  document.getElementById("thumb4");
+var thumb4Src =  document.getElementById("thumb4").src;
+var thumb5 =  document.getElementById("thumb5");
+var thumb5Src =  document.getElementById("thumb5").src;
+var thumb6 =  document.getElementById("thumb6");
+var thumb6Src =  document.getElementById("thumb6").src;
+var thumb7 =  document.getElementById("thumb7");
+var thumb7Src =  document.getElementById("thumb7").src;
+
+thumb1.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb1Src
+})
+thumb2.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb2Src
+})
+thumb3.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb3Src
+})
+thumb4.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb4Src
+})
+thumb5.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb5Src
+})
+thumb6.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb6Src
+})
+thumb7.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb7Src
+})
